7566d70e15b508193fbfeb1a10920c8ef2582645
VOjha <vidushi.ojha@gmail.com>
2016-11-02 16:17:39 -0700

    Avoid uploading data file

--- classifier_test.py
+++ classifier_test.py
@@ -1,25 +1,53 @@
 import parser
+import make_sets
 from pprint import pprint
 from naiveBayesClassifier import tokenizer
 from naiveBayesClassifier.trainer import Trainer
 from naiveBayesClassifier.classifier import Classifier
 
 
-def classify(filename):
+def classify(filename, size):
 
-    trainingSet = parser.parse(filename)
+    trainingSet, testingSet = make_sets.create_sets(filename, size)
 
     trainer = Trainer(tokenizer.Tokenizer(stop_words = [], signs_to_remove = [""]))
+
 
     for sample in trainingSet:
         trainer.train(sample['url'], sample['result'])
 
     classifier = Classifier(trainer.data, tokenizer.Tokenizer(stop_words = [], signs_to_remove = [""]))
 
-    newInstance = u'https://secure.actblue.com/contribute/page/dcccactblue?amount=25&recurring=true'
+    mal_mal = 0
+    mal_clean = 0
+    clean_clean = 0
+    clean_mal = 0
 
-    print classifier.classify(newInstance)
+    for sample in testingSet:
+
+    	predicted = classifier.classify(sample['url'])[0][0]
+    	actual = sample['result']
+
+    	if predicted == 'malicious' and actual == 'malicious':
+    		mal_mal += 1
+    	elif predicted == 'malicious' and actual == 'clean':
+    		mal_clean += 1
+    	elif predicted == 'clean' and actual == 'clean':
+    		clean_clean += 1
+    	elif predicted == 'clean' and actual == 'malicious':
+    		clean_mal += 1
+
+    size = float(size)
+
+    mal_mal = float(mal_mal)/size
+    mal_clean = float(mal_clean)/size
+    clean_mal = float(clean_mal)/size
+    clean_clean = float(clean_clean)/size
+
+    confusionMatrix = [[mal_mal, clean_mal], [mal_clean, clean_clean]]
+
+    pprint(confusionMatrix)
 
 
-def test():
-    classify('sample.txt')
+def test(size):
+    classify('lotsodata.txt', size)
