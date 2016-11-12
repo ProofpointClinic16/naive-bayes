81d4a2ff7349c001db246ab59b36147ef04c501d
kherrera <kherrera@hmc.edu>
2016-11-11 18:56:51 -0800

    Tokenizer enhancements

--- tokenizer.py
+++ tokenizer.py
@@ -6,9 +6,15 @@ def tokenize(filename):
 
     tokens_slash = []
     tokens_period = []
+    tokens_sp = []
 
+    #Calls parse function from parser file
+    #and stores parsed data in 'data' 
     data = parser.parse(filename)
 
+    #This block of code tokenizes the URLs
+    #that exist in 'data' by '/' and '.' and
+    #creates a list to print out later
     for i in xrange(len(data)):
         slash_split = data[i]['url'].split('/')
         period_split = data[i]['url'].split('.')
@@ -16,11 +22,25 @@ def tokenize(filename):
         tokens_slash += [slash_split]
         tokens_period += [period_split]
 
+    #Separate block of code that takes the list
+    #holding the URLs tokenized by slashes and
+    #then tokenizes them further by periods.
+    for i in xrange(len(tokens_slash)):
+        for j in xrange(len(tokens_slash[i])):
+            sp_split = tokens_slash[i][j].split('.')
+
+            #Issue here when creating a LoL
+            #URLs are not separated into lists
+            tokens_sp += sp_split
+
     print "SLASHES"
     pprint(tokens_slash)
     print "--------------------------------"
     print "PERIODS"
     pprint(tokens_period)
+    print "--------------------------------"
+    print "SLASHES AND PERIODS"
+    pprint(tokens_sp)
 
 
 def test():
